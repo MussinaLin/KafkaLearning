338ab02b5a69f3523336d7f7e6e5553c3c956ecb
Mussina Lin <mussina.lin@nogle.com>
2019-05-26 15:12:27 +0800

    send data to kafka by KafkaProducer

--- src/main/java/com/mussina/self/kafkacourse/ProducerDemo.java
+++ src/main/java/com/mussina/self/kafkacourse/ProducerDemo.java
@@ -1,7 +1,31 @@
 package com.mussina.self.kafkacourse;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
     public static void main(String[] args) {
-        System.out.println("hello !");
+        String bootstrap_servers = "127.0.0.1:9092";
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap_servers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create producer
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        // create ProducerRecord
+        ProducerRecord<String,String> record = new ProducerRecord<String, String>("first_topic", "hello kafka producer!");
+
+        // send data
+        producer.send(record);
+
+        //flush and close
+        producer.close();
     }
 }
