afc0292786a399377897d2b472ef7f88a0e0664b
Mussina Lin <mussina.lin@nogle.com>
2019-06-08 12:46:59 +0800

    Kafka idempotent producer

--- src/main/java/com/mussina/self/kafkacourse/twitter/TwitterProducer.java
+++ src/main/java/com/mussina/self/kafkacourse/twitter/TwitterProducer.java
@@ -109,6 +109,11 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG,"all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5");
+
         // create producer
         KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
         return producer;
