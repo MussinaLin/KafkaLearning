ee2394c53649690627d7eac894bb0fe5081eb2ab
Mussina Lin <mussina.lin@nogle.com>
2019-07-07 22:03:42 +0800

    filter tweets which are followed more than 10000 followers

--- kafka-stream-filter-tweet/src/main/java/mussina/kafka/course/StreamsFilterTweets.java
+++ kafka-stream-filter-tweet/src/main/java/mussina/kafka/course/StreamsFilterTweets.java
@@ -1,12 +1,52 @@
 package mussina.kafka.course;
 
+import com.google.gson.JsonParser;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+
 public class StreamsFilterTweets {
-    //create properties
+    private static JsonParser jsonParser = new JsonParser();
+    public static void main(String[] args) {
+        //create properties
+        Properties properties = new Properties();
+        properties.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1");
+        properties.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "demo-kafka-streams");
+        properties.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
+        properties.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.StringSerde.class.getName());
 
-    //create topology
+        //create topology
+        StreamsBuilder streamsBuilder = new StreamsBuilder();
 
-    //build topology
+        //input topic
+        KStream<String, String> inputTopic = streamsBuilder.stream("twitter_tweets");
+        KStream<String, String> filterStream = inputTopic.filter(
+            (k,v)-> extractFollowerinTweet(v) > 10000// filter for tweets which has more than 10000 follower
+        );
+        filterStream.to("important_tweets");
 
-    //start stream application
+        //build topology
+        KafkaStreams kafkaStreams = new KafkaStreams(
+            streamsBuilder.build(),
+            properties
+        );
+
+        //start stream application
+        kafkaStreams.start();
+    }
+
+    private static int extractFollowerinTweet(String tweetJson){
+        try {
+            return jsonParser.parse(tweetJson).getAsJsonObject().get("user")
+                .getAsJsonObject().get("followers_count").getAsInt();
+        }catch (NullPointerException e){
+            return 0;
+        }
+    }
+
 
 }
